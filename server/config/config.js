391442d54c092fc4bfac1b25da2aed24d40b23e7
David Rojas <deznorth@gmail.com>
2019-04-11 14:15:58 -0400

    fix config

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {};
 
-config.isDev = process.env.NODE_ENV !== 'production';
+config.isDev = process.env.NODE_ENV != 'production';
 config.PORT = this.isDev ? process.env.PORT : 5000;
 
 module.exports = config;
